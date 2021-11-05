2010c2f44e404f01e8b2a4aa460092402634f750
John Dao <john.d1729@gmail.com>
2021-11-05 22:56:57 +1300

    learning react still

--- front-end/src/index.js
+++ front-end/src/index.js
@@ -1,13 +1,30 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);      
-reportWebVitals();
+import './index.css'
+
+const books =[{
+  img:'https://',
+  title:'Love you',
+  author:'mommy'
+},{
+  author: 'John Doe',
+  title: 'Unknown',
+  img: 'http://'
+}]
+
+function BookList(){
+  return <section>
+    {books}
+  </section>
+}
+
+const Book = ({img,title,author})=>{
+  return <article className="book">
+    <img src={img} alt='book'/>
+    <h1>{title}</h1>
+    <h4>{author}</h4>
+  </article>
+}
+
+ReactDOM.render(<BookList/>, document.getElementById('root'));
