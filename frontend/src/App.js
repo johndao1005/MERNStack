2d68b688e0218321e1b9fd2a96501ec1eba22f36
John Dao <john.d1729@gmail.com>
2021-11-10 09:39:10 +1300

    follow piush-eon tutorial, just install react-bootstrap

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,36 +1,15 @@
 import './App.css';
-import {useState} from 'react'
-import {BrowserRouter as Router, Switch, Route} from 'react-router-dom'
-
-//import screens
-import HomeScreen from './screens/HomeScreen';
-import ProductScreen from './screens/ProductScreen';
-import CartScreen from './screens/CartScreen';
-
-//import components
-import NavBar from './components/NavBar'
-import SideDrawer from './components/SideDrawer'
-import Backdrop from './components/Backdrop'
+import Footer from './components/Footer/Footer';
+import Header from './components/Header/Header';
 
 function App() {
-  const [sideToogle,setSideToggle] = useState(false);
   return (
-    <Router>
-      <NavBar click={()=> setSideToggle(true)}/>
-      <SideDrawer show={sideToogle} click={()=> setSideToggle(false)} />
-      <Backdrop show={sideToogle} click={()=> setSideToggle(false)}/>
-      <main>
-        <Switch>
-          <Route exact path="/" component={HomeScreen}/>
-          <Route exact path="/product/:id" component={ProductScreen}/>
-          <Route exact path="/cart" component={CartScreen}/>
-        </Switch>
-      </main>
-      {/* HomeScreen */}
-      {/* ProductScreen */}
-      {/* CartScreen */}
-    </Router>
-  );
+    <>
+      <Header/>
+      {/* <main style=({minHeight:"93vh"})></main> */}
+      <Footer/>
+    </>
+  )
 }
 
 export default App;
--- frontend/src/components/Backdrop.js
+++
@@ -1,7 +0,0 @@
-import './Backdrop.css'
-
-const Backdrop = ({show,click}) => {
-    return show && <div className="backdrop"  onClick={click}/>
-}
-
-export default Backdrop
