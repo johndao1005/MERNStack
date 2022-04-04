6607f11a0ece05165df66ee4ad898a91b5e4c3ac
johndao1005 <john.d1729@gmail.com>
2022-04-04 16:10:08 +1200

    install server and front end backbone

---
+++ Server-side/seeder/seeder.js
@@ -0,0 +1,28 @@
+require('dotenv').config();
+const connectDB = require("../config/db")
+const {User} = require("../models/user")
+const userData = require("../data/usersData")
+const {Token,Product} = require("../models/transaction")
+const tokenData = require("../data/tokenData")
+const productData = require("../data/productsData")
+
+connectDB();
+
+const importData = async() => {
+    try {
+        await User.deleteMany({})
+        await User.insertMany(userData)
+        await Token.deleteMany({})
+        await Token.insertMany(tokenData)
+        await Product.deleteMany({})
+        await Product.insertMany(productData)
+        console.log('Import success')
+        process.exit(0)
+
+    } catch (e) {
+        console.error(`Error with user import ${e}`)
+        process.exit(1)
+    }
+}
+
+importData()
---
+++ Server-side/server.js
@@ -0,0 +1,40 @@
+//ANCHOR Preparation
+// reading secrets key
+require('dotenv').config();
+// import modules
+const express = require("express")
+const path = require("path");
+const morgan = require("morgan")
+
+// Api routes
+const userRoutes = require("./routes/userRoutes");
+const transactionRoutes = require("./routes/transactionRoutes");
+
+//import middleware to handle error
+//const{notFound,errorHandler}= require("./middleware/errorMiddleware")
+
+// connect to MongoDB
+const connectDB = require("./config/db")
+
+//ANCHOR start the server
+connectDB() //connect to database with base log in
+
+const app = express();// start an instance of the app
+
+// read json data and url code
+app.use(express.json()) 
+app.use(express.urlencoded({ extended: true }));
+
+if (process.env.NODE_ENV === 'development') {
+    app.use(morgan('dev'))
+  }
+  
+
+// Routes list
+app.use("/user", userRoutes)
+app.use("/", transactionRoutes)
+
+
+const PORT = process.env.PORT || 3000 // if reading .env file fail => running on port 3000
+
+app.listen(PORT, console.log( `Server running in ${process.env.NODE_ENV} mode on port ${PORT}`))
